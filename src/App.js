8cfe03c50d9ceaf23b0fbdc2c430cf492ef362af
sandesh <sandeshbhatta218@gmail.com>
2025-03-10 12:39:06 +0545

    Search query component is added.

--- src/App.js
+++ src/App.js
@@ -62,4 +62,15 @@ function Header() {
     );
 }
 
+function SearchPosts() {
+    const { searchQuery, setSearchQuery } = usePosts();
+    return (
+        <input
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search posts..."
+        />
+    );
+}
+
 export default App;
