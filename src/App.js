540fc911eddf248102b7b2e64693afc68e0c2546
sandesh <sandeshbhatta218@gmail.com>
2025-03-12 12:59:58 +0545

    new post adding funtion and list of post component is added.

--- src/App.js
+++ src/App.js
@@ -96,4 +96,52 @@ function Posts() {
     );
 }
 
+
+function FormAddPost() {
+    const { onAddPost } = usePosts();
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    const handleSubmit = function (e) {
+        e.preventDefault();
+        if (!body || !title) return;
+        onAddPost({ title, body });
+        setTitle("");
+        setBody("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Post title"
+            />
+            <textarea
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                placeholder="Post body"
+            />
+            <button>Add post</button>
+        </form>
+    );
+}
+
+function List() {
+    const { posts } = usePosts();
+    return (
+        <>
+            <ul>
+                {posts.map((post, i) => (
+                    <li key={i}>
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                    </li>
+                ))}
+            </ul>
+            {/* <Test /> */}
+        </>
+    );
+}
+
 export default App;
