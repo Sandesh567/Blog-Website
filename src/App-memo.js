cdd40bff81759c51b530c925b004400fb6a18ca3
sandesh <sandeshbhatta218@gmail.com>
2025-03-15 12:27:03 +0545

    Importing hooks and faker.js

--- src/App-memo.js
+++ src/App-memo.js
@@ -1,3 +1,15 @@
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+    return {
+        title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+        body: faker.hacker.phrase(),
+    };
+}
+
+
+
 function App() {
     const [posts, setPosts] = useState(() =>
         Array.from({ length: 30 }, () => createRandomPost())
