4207b6ce20ba4da87099f43e9353a37a89c5d3cb
sandesh <sandeshbhatta218@gmail.com>
2025-03-15 12:26:31 +0545

    Adding the main app component

---
+++ src/App-memo.js
@@ -0,0 +1,61 @@
+function App() {
+    const [posts, setPosts] = useState(() =>
+        Array.from({ length: 30 }, () => createRandomPost())
+    );
+    const [searchQuery, setSearchQuery] = useState("");
+    const [isFakeDark, setIsFakeDark] = useState(false);
+
+    // Derived state. These are the posts that will actually be displayed
+    const searchedPosts =
+        searchQuery.length > 0
+            ? posts.filter((post) =>
+                `${post.title} ${post.body}`
+                    .toLowerCase()
+                    .includes(searchQuery.toLowerCase())
+            )
+            : posts;
+
+    const handleAddPost = useCallback(function handleAddPost(post) {
+        setPosts((posts) => [post, ...posts]);
+    }, [])
+
+    function handleClearPosts() {
+        setPosts([]);
+    }
+
+    // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+    useEffect(
+        function () {
+            document.documentElement.classList.toggle("fake-dark-mode");
+        },
+        [isFakeDark]
+    );
+
+    const archiveOptions = useMemo(() => {
+        return {
+            show: false,
+            title: `Post archive in addition to ${posts.length} main post`,
+        };
+    }, [posts.length]);
+
+    return (
+        <section>
+            <button
+                onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+                className="btn-fake-dark-mode"
+            >
+                {isFakeDark ? "☀️" : "🌙"}
+            </button>
+
+            <Header
+                posts={searchedPosts}
+                onClearPosts={handleClearPosts}
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+            />
+            <Main posts={searchedPosts} onAddPost={handleAddPost} />
+            <Archive archiveOptions={archiveOptions} onAddPost={handleAddPost} />
+            <Footer />
+        </section>
+    );
+}
