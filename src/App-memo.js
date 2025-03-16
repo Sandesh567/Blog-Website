20bb7e08f3e43b52df5967d832b8a1857981e6aa
sandesh <sandeshbhatta218@gmail.com>
2025-03-16 13:04:19 +0545

    Adding header component.

--- src/App-memo.js
+++ src/App-memo.js
@@ -71,3 +71,23 @@ function App() {
         </section>
     );
 }
+
+
+
+function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
+    return (
+        <header>
+            <h1>
+                <span>⚛️</span>The Atomic Blog
+            </h1>
+            <div>
+                <Results posts={posts} />
+                <SearchPosts
+                    searchQuery={searchQuery}
+                    setSearchQuery={setSearchQuery}
+                />
+                <button onClick={onClearPosts}>Clear posts</button>
+            </div>
+        </header>
+    );
+}
