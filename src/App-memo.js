a795fb8f5bb48b0aec10859819320b79e47b65a6
sandesh <sandeshbhatta218@gmail.com>
2025-03-18 11:36:09 +0545

    Showing the post list .

--- src/App-memo.js
+++ src/App-memo.js
@@ -154,6 +154,20 @@ function FormAddPost({ onAddPost }) {
 }
 
 
+function List({ posts }) {
+    return (
+        <ul>
+            {posts.map((post, i) => (
+                <li key={i}>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+
 
 
 
